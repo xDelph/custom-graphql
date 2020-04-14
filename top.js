4cdd5cafc9b98f35c9662ee5873adce3c37c9108
Thomas Delalonde <thomas.delalonde@gmail.com>
2020-04-14 13:14:29 +0200

    feat: update how to lunch the script

--- top.js
+++ top.js
@@ -14,16 +14,16 @@ async function callApi (query) {
   return res.data.data
 }
 
-function getRepoQuery (first, after) {
+function getRepoQuery (name, owner, first, after) {
   return `
     {
-      repository(name: "giVR", owner: "bramis") {
+      repository(name: "${name}", owner: "${owner}") {
         name
         defaultBranchRef {
           name
           target {
             ... on Commit {
-              history(first: ${first}${after ? `, after: ${after}` : ''}) {
+              history(first: ${first}${after ? `, after: "${after}"` : ''}) {
                 pageInfo {
                   hasNextPage
                   endCursor
@@ -50,11 +50,11 @@ function getRepoQuery (first, after) {
   `
 }
 
-function getIssues (first, after) {
+function getIssues (name, owner, first, after) {
   return `
     {
-      repository(name: "giVR", owner: "bramis") {
-        issues(first: ${first}${after ? `, after: ${after}` : ''}) {
+      repository(name: "${name}", owner: "${owner}") {
+        issues(first: ${first}${after ? `, after: "${after}"` : ''}) {
           pageInfo{
             endCursor
             hasNextPage
@@ -78,11 +78,18 @@ function getRepoHistory (data) {
 }
 
 (async function () {
-  const repoData = (await callApi(getRepoQuery(100))).repository
+  if (process.argv.length < 4) {
+    console.error(`need a command line like : node top.js repoName repoOwner`)
+    process.exit(-1)
+  }
+  const repoName = process.argv[2]
+  const repoOwner = process.argv[3]
+
+  const repoData = (await callApi(getRepoQuery(repoName, repoOwner, 100))).repository
 
   while (repoData.defaultBranchRef.target.history.pageInfo.hasPageNext) {
     const history = getRepoHistory(repoData)
-    const nextRepoData = (await callApi(getRepoQuery(100, history.pageInfo.endCursor))).repository
+    const nextRepoData = (await callApi(getRepoQuery(repoName, repoOwner, 100, history.pageInfo.endCursor))).repository
     history.nodes.push(...getRepoHistory(nextRepoData).nodes)
     history.pageInfo = getRepoHistory(nextRepoData).pageInfo
   }
@@ -101,10 +108,10 @@ function getRepoHistory (data) {
   const sortedValues = Object.entries(nbCommitsPerUser).sort(([, a], [, b]) => b - a)
   const topTwoUser = sortedValues.slice(0, 2).map(x => x[0])
 
-  const issues = (await callApi(getIssues(100))).repository.issues
+  const issues = (await callApi(getIssues(repoName, repoOwner, 100))).repository.issues
 
   while (issues.pageInfo.hasPageNext) {
-    const nextIssuesData = (await callApi(getIssues(100, issues.pageInfo.endCursor))).repository.issues
+    const nextIssuesData = (await callApi(getIssues(repoName, repoOwner, 100, issues.pageInfo.endCursor))).repository.issues
     issues.edges.push(nextIssuesData.edges)
     issues.pageInfo.pageInfo = nextIssuesData.pageInfo
   }
